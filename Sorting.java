942533331fa10547337651c1f3378736da226300
sakshigmail <sakshi3016kaurav.com>
2025-03-11 02:45:32 +0530

    adding sorting file

---
+++ Sorting.java
@@ -0,0 +1,50 @@
+public class Sorting{
+
+    public static void main(String[] args){
+        int nums[] = {1,5,2,8,9,4};
+        int size = nums.length;
+        int temp = 0;
+
+        System.out.println("before sorting");
+        for(int num : nums){
+            System.out.print(num + " ");
+        }
+
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size-1;j++){
+                if(nums[j] > nums[j+1]){
+                    temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                    
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+        System.out.println();
+        System.out.println("after sorting");
+        for(int num : nums){
+            System.out.print(num + " ");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
