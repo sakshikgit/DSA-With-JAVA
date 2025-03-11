200f958c782f1981235c7886ab05f6fc8b58820a
sakshigmail <sakshi3016kaurav.com>
2025-03-11 21:41:41 +0530

    adding quick sort

--- Sorting.java
+++ Sorting.java
@@ -73,27 +73,77 @@ public class Sorting{
     // selection sort starts here
 
 
-    public static void main(String[] args){
-        int nums[] = {5,6,2,3,1};
-        int size = nums.length;
+    // public static void main(String[] args){
+    //     int nums[] = {5,6,2,3,1};
+    //     int size = nums.length;
 
-        for(int i=1;i<size;i++){
-            int key = nums[i];
-            int j = i-1;
+    //     for(int i=1;i<size;i++){
+    //         int key = nums[i];
+    //         int j = i-1;
 
-            while(j>=0 && nums[j] > key){
-                nums[j+1] = nums[j];
-                j--;
+    //         while(j>=0 && nums[j] > key){
+    //             nums[j+1] = nums[j];
+    //             j--;
+    //         }
+    //         nums[j+1] = key;
+
+    //     }
+
+    //     for(int num : nums)
+    //     {
+    //         System.out.print(num + " ");
+    //     }
+    // }
+
+
+
+    // Quick sort starts here
+
+    public static void quickSort(int[] arr, int low, int high)
+    {
+        if(low<high)
+        {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi-1);
+            quickSort(arr, pi+1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high){
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for(int j=low; j<high; j++)
+        {
+            if(arr[j] < pivot)
+            {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
-            nums[j+1] = key;
 
         }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
 
-        for(int num : nums)
+        return i+1;
+    }
+
+    public static void main(String[] args){
+        int arr[] = {5,6,2,3,1,8,4};
+
+        quickSort(arr, 0, arr.length-1);
+
+
+        for(int num : arr)
         {
             System.out.print(num + " ");
         }
     }
+
 
 
 
