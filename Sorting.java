09ffd6cd2da877fbb2662024bc82a8f7e8f5e781
sakshigmail <sakshi3016kaurav.com>
2025-03-11 15:36:02 +0530

    adding selection sort

--- Sorting.java
+++ Sorting.java
@@ -1,29 +1,63 @@
 public class Sorting{
 
+    // Bubble sort starts here
+
+    // public static void main(String[] args){
+    //     int nums[] = {1,5,2,8,9,4};
+    //     int size = nums.length;
+    //     int temp = 0;
+
+    //     System.out.println("before sorting");
+    //     for(int num : nums){
+    //         System.out.print(num + " ");
+    //     }
+
+    //     for(int i=0;i<size;i++){
+    //         for(int j=0;j<size-1;j++){
+    //             if(nums[j] > nums[j+1]){
+    //                 temp = nums[j];
+    //                 nums[j] = nums[j+1];
+    //                 nums[j+1] = temp;
+                    
+    //             }
+    //         }
+    //     }
+
+    //     System.out.println();
+    //     System.out.println("after sorting");
+    //     for(int num : nums){
+    //         System.out.print(num + " ");
+    //     }
+    // }
+
+
+    // selection sort starts here
+
     public static void main(String[] args){
-        int nums[] = {1,5,2,8,9,4};
+        int nums[] = {6,5,2,8,9,4};
         int size = nums.length;
         int temp = 0;
+        int minIndex = -1;
 
         System.out.println("before sorting");
         for(int num : nums){
-            System.out.print(num + " ");
+            System.out,print(num + " ");
         }
 
-        for(int i=0;i<size;i++){
-            for(int j=0;j<size-1;j++){
-                if(nums[j] > nums[j+1]){
-                    temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
-                    
+
+        for(int i=0;i<size-1;i++){
+            for(int j=i+1;j<size;j++){
+                if(nums[minIndex] > nums[j]){
+                    minIndex = j;
                 }
+
+            temp = nums[minIndex];
+            nums[minIndex] = nums[i];
+            nums[i] = nums[minIndex];
+
+
             }
         }
-
-
-
-
 
 
 
@@ -31,9 +65,12 @@ public class Sorting{
         System.out.println();
         System.out.println("after sorting");
         for(int num : nums){
-            System.out.print(num + " ");
+            System.out,print(num + " ");
         }
+
+
     }
+
 
 
 
