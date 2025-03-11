0648060b3697aafbbb3fa62cbd5c84aaf7120d31
sakshigmail <sakshi3016kaurav.com>
2025-03-11 22:27:59 +0530

    adding merge sort

--- Sorting.java
+++ Sorting.java
@@ -99,49 +99,129 @@ public class Sorting{
 
     // Quick sort starts here
 
-    public static void quickSort(int[] arr, int low, int high)
-    {
-        if(low<high)
-        {
-            int pi = partition(arr, low, high);
+    // public static void quickSort(int[] arr, int low, int high)
+    // {
+    //     if(low<high)
+    //     {
+    //         int pi = partition(arr, low, high);
 
-            quickSort(arr, low, pi-1);
-            quickSort(arr, pi+1, high);
+    //         quickSort(arr, low, pi-1);
+    //         quickSort(arr, pi+1, high);
+    //     }
+    // }
+
+    // private static int partition(int[] arr, int low, int high){
+    //     int pivot = arr[high];
+    //     int i = low - 1;
+
+    //     for(int j=low; j<high; j++)
+    //     {
+    //         if(arr[j] < pivot)
+    //         {
+    //             i++;
+    //             int temp = arr[i];
+    //             arr[i] = arr[j];
+    //             arr[j] = temp;
+    //         }
+
+    //     }
+    //     int temp = arr[i+1];
+    //     arr[i+1] = arr[high];
+    //     arr[high] = temp;
+
+    //     return i+1;
+    // }
+
+    // public static void main(String[] args){
+    //     int arr[] = {5,62,2,3,111,81,4};
+
+    //     quickSort(arr, 0, arr.length-1);
+
+
+    //     for(int num : arr)
+    //     {
+    //         System.out.print(num + " ");
+    //     }
+    //}
+
+
+
+
+    // merge sort starts here 
+
+
+    private static void mergeSort(int[] arr, int l, int r){
+
+        if(l<r){
+            int mid = (l+r)/2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid+1, r);
+
+            merge(arr, l, mid, r);
         }
+
     }
 
-    private static int partition(int[] arr, int low, int high){
-        int pivot = arr[high];
-        int i = low - 1;
+    private static void merge(int[] arr, int l, int mid, int r){
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
 
-        for(int j=low; j<high; j++)
-        {
-            if(arr[j] < pivot)
-            {
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+        int lArr[] = new int[n1];
+        int rArr[] = new int[n2];
 
+        for(int x=0; x<n1; x++){
+            lArr[x] = arr[l+x];
         }
-        int temp = arr[i+1];
-        arr[i+1] = arr[high];
-        arr[high] = temp;
+        for(int x=0; x<n2; x++){
+            rArr[x] = arr[mid+1+x];
+        }
 
-        return i+1;
+        int i=0;
+        int j=0;
+        int k=l;
+
+        while(i<n1 && j<n2){
+            if(lArr[i] <= rArr[j]){
+                arr[k] = lArr[i];
+                i++;
+            }
+            else{
+                arr[k] = rArr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<n1){
+            arr[k] = lArr[i];
+            i++;
+            k++;
+        }
+
+        while(i<n2){
+            arr[k] = rArr[i];
+            j++;
+            k++;
+        }
+
     }
 
     public static void main(String[] args){
-        int arr[] = {5,6,2,3,1,8,4};
+        int arr[] = {3,5,1,4,6,2};
 
-        quickSort(arr, 0, arr.length-1);
-
-
-        for(int num : arr)
-        {
-            System.out.print(num + " ");
+        for(int n : arr){
+            System.out.print(n + " ");
         }
+        System.out.println();
+
+
+
+
+        System.out.println("After sorting");
+        for(int n : arr){
+            System.out.print(n + " ");
+        }
+
+        
     }
 
 
